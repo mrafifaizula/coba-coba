35928d76c0ba949872d9d267bed672fc4cbd8499
unknown <mrafifaizula@gmail.com>
2024-04-19 15:36:34 +0700

    push project laravel

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// router basic
+Route::get('/about', function () {
+    return '<h1>Halo</h1>'
+    . 'Selamat datang di webapp saya<br>'
+    . 'Te kuattt laparr hayang baso di sanguannn.';
+});
+
+// router baisc  passing data to view
+Route::get('animals', function () {
+    $king = "lion";
+    $hewan = ['monkey', 'dragonfly', 'tiger', 'butterfly', 'crocodile'];
+    return view('animals_page', compact('king', 'hewan'));
+});
